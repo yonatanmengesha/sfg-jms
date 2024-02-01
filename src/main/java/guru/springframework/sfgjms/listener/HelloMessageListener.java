a5c6dd2ed4a6c7f20e3fa644a442a97aaaccc1d8
Yonatan Mengesha <yonatan.mengesha2017@gmail.com>
2024-02-01 14:23:15 -0500

    commit after adding a message listener

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+@JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+    System.out.println("I Got A Message!!!!");
+    System.out.println(helloWorldMessage);
+
+   // throw new RuntimeException("foo");
+    }
+}
